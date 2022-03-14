6e5f376482218bde6b95136d0c213fce3e077932
redred <imbapalagz@gmail.com>
2022-03-14 21:50:12 +0800

    changed footer link

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,7 @@ import './footer.css'
 const Footer = () => {
 	return (
 		<footer>
-			&copy; 2022. Porfolio of Reuben Daniel E. Domondon. Made using ReactJS with HTML and CSS. See source code <a href="https://github.com/zerathemant/my-portfolio-react" target="_blank" className="github-link">here.</a>
+			&copy; 2022. Porfolio of Reuben Daniel E. Domondon. Made using ReactJS with HTML and CSS. See source code <a href="https://github.com/zerathemant/my-portfolio" target="_blank" className="github-link">here.</a>
 		</footer>
 	)
 }
